2fc49aeb36a3dd4157afe047c0a506440b377ca6
rBaku <rodolfoeduardoov@gmail.com>
2025-10-13 02:41:47 -0400

    movimiento personaje y fisicas de murallas

---
+++ scenes/characters/mc/mc.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+const tile_size: Vector2 = Vector2(16,16)
+var sprite_node_pos_tween: Tween
+
+func _ready():
+	add_to_group("player")
+
+func _physics_process(_delta: float) -> void:
+	if !sprite_node_pos_tween or !sprite_node_pos_tween.is_running():
+		if Input.is_action_just_pressed("ui_up") and !$Up.is_colliding():
+			_move(Vector2(0,-1))
+		elif Input.is_action_just_pressed("ui_down") and !$Down.is_colliding():
+			_move(Vector2(0,1))
+		elif Input.is_action_just_pressed("ui_left") and !$Left.is_colliding():
+			_move(Vector2(-1,0))
+		elif Input.is_action_just_pressed("ui_right") and !$Right.is_colliding():
+			_move(Vector2(1,0))
+func _move(dir: Vector2):
+	global_position += dir * tile_size
+	$Sprite2D.global_position -= dir * tile_size
+	
+	if sprite_node_pos_tween:
+		sprite_node_pos_tween.kill()
+	sprite_node_pos_tween = create_tween()
+	sprite_node_pos_tween.set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
+	sprite_node_pos_tween.tween_property($Sprite2D, "global_position", global_position, 0.2).set_trans(Tween.TRANS_SINE)
