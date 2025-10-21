cc6440905d1bf29abd3ceb459adcd4e4d506ecf8
cvarela3344 <claudio.varela@usm.cl>
2025-10-21 01:58:00 -0300

    arreglo de menu

--- scenes/UI/level_selector/level_selector.gd
+++ scenes/UI/level_selector/level_selector.gd
@@ -3,4 +3,4 @@ extends Control
 
 
 func _on_button_pressed():
-	UiController.load_scene("/levels/level_1/","level_1","fade_to_black")
+	get_tree().change_scene_to_file("res://scenes/levels/level_1/level_1.tscn")
--- scenes/UI/main_menu/menu.gd
+++ scenes/UI/main_menu/menu.gd
@@ -2,4 +2,4 @@ extends Control
 
 
 func _on_button_pressed():
-	UiController.load_scene("/UI/level_selector/","level_selector", "fade_to_black")
+	get_tree().change_scene_to_file("res://scenes/UI/level_selector/level_selector.tscn")
