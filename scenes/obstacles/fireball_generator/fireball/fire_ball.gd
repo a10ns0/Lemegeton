193a28a8b1d79cb11ddb5412ba3de927d9179aed
a1ons00 <alonso.castillop@usm.cl>
2025-11-02 01:43:19 -0300

    animacion fireball y personaje principal

--- scenes/obstacles/fireball_generator/fireball/fire_ball.gd
+++ scenes/obstacles/fireball_generator/fireball/fire_ball.gd
@@ -11,13 +11,13 @@ func _on_fire_turn():
 
 func _move(dir: Vector2):
 	global_position += dir * tile_size
-	$Sprite2D.global_position -= dir * tile_size
+	$AnimatedSprite2D.global_position -= dir * tile_size
 	
 	if sprite_node_pos_tween:
 		sprite_node_pos_tween.kill()
 	sprite_node_pos_tween = create_tween()
 	sprite_node_pos_tween.set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
-	sprite_node_pos_tween.tween_property($Sprite2D, "global_position", global_position, 0.2).set_trans(Tween.TRANS_SINE)
+	sprite_node_pos_tween.tween_property($AnimatedSprite2D, "global_position", global_position, 0.2).set_trans(Tween.TRANS_SINE)
 
 
 
