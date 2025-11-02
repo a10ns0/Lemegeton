5e9f638e196e6373603313d977b402c5cfee355c
cvarela3344 <claudio.varela@usm.cl>
2025-11-02 02:55:19 -0300

    levels enlazados

--- scenes/UI/level_selector/level_selector.gd
+++ scenes/UI/level_selector/level_selector.gd
@@ -1,6 +1,4 @@
 extends Control
 
-
-
-func _on_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/levels/level_1/level_1.tscn")
+func _on_button_pressed(path):
+	get_tree().change_scene_to_file(path)
